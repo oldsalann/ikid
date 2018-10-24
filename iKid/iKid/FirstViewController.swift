2eff1ac371bd5f45c8fe32c1c3d35d4e9f578033
Nicholas Olds <oldsn@D-10-19-139-54.dhcp4.washington.edu>
2018-10-24 08:41:56 -0700

    added base file

---
+++ iKid/iKid/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  iKid
+//
+//  Created by Nicholas Olds on 10/24/18.
+//  Copyright © 2018 Nicholas Olds. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iKid/iKid/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iKid
+//
+//  Created by Nicholas Olds on 10/24/18.
+//  Copyright © 2018 Nicholas Olds. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
