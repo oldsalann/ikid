6431f53b7531094e579c34c124639ce9adfa7371
Nicholas Olds <oldsn@D-10-19-139-54.dhcp4.washington.edu>
2018-10-24 09:08:21 -0700

    added third view

---
+++ iKid/iKid/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  iKid
+//
+//  Created by Nicholas Olds on 10/24/18.
+//  Copyright © 2018 Nicholas Olds. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
